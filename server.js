7ac9ed4bbc5e4fc315439ef226eb170256971885
Tony Isidro <tonyi@Tonys-MBP-3.fios-router.home>
2018-05-21 01:38:01 -0400

    Updated files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
